fb49022e3b93a4eaaae2e7a299de2346fee6c6a7
Alejandra Reynoso <alejandrareinosoh@gmail.com>
2022-04-02 16:38:01 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const {
+    getAllHouses,
+    createHouse,
+    updateHouse,
+    deleteHouse
+} = require('./controller')
+
+app.get(`/api/houses`, getAllHouses)
+app.post(`/api/houses`, createHouse)
+app.put(`/api/houses/:id`, updateHouse)
+app.delete(`/api/houses/:id`, deleteHouse)
+
+
+app.listen(4004, () => console.log('Server running on 4004'));
